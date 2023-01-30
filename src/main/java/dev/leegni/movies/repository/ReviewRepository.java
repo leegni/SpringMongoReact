72b75ae2b6290320bedcbbeb3cb4809469c7cbda
Geunhee Lee <igni950424@gmail.com>
2023-01-31 00:09:30 +0900

    Initial commit

---
+++ src/main/java/dev/leegni/movies/repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package dev.leegni.movies.repository;
+
+import dev.leegni.movies.domain.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
